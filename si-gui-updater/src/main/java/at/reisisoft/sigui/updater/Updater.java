7270796d99da4bdf4c1d245acedb91ea077b3db4
Florian Reisinger <reisi007@gmail.com>
2018-04-25 19:24:10 +0200

    Improved updater

--- si-gui-updater/src/main/java/at/reisisoft/sigui/updater/Updater.java
+++ si-gui-updater/src/main/java/at/reisisoft/sigui/updater/Updater.java
@@ -13,10 +13,12 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Updater {
+    private static boolean isWindows = System.getProperty("os.name").toLowerCase().contains("wind");
 
     private static final String updaterUrl = "https://tdf.io/kotlinsiguiupdaterendpoint";
     private static final Path siGuiInstallFolder = Paths.get(".", "kotlin-si-gui").toAbsolutePath().normalize();
     private static final String settingFilename = "si-gui.settings.json";
+    private static final Path scriptFilePath = siGuiInstallFolder.resolve("si-gui-desktop").resolve("bin").resolve("si-gui-desktop");
 
     public static void main(String[] args) {
         try {
@@ -26,56 +28,63 @@ public class Updater {
                 //Same eTag -> no update needed
                 startSiGui();
             } else {
-                final Path tmpFile = Paths.get(".", online.getKey() + ".zip");
-                Files.deleteIfExists(tmpFile);
-                //TODO show progress / Ask to dowenload
-                try {
-                    try (final InputStream onlineIn = online.getValue().getInputStream();
-                         final OutputStream fileOut = Files.newOutputStream(tmpFile, StandardOpenOption.CREATE_NEW)) {
-                        final float length = online.getValue().getContentLength();
-                        int totalRead = 0;
-                        int read;
-                        float realPercentage;
-                        byte[] buffer = new byte[1024 * 32];
-                        while ((read = onlineIn.read(buffer)) >= 0) {
-                            totalRead += read;
-                            fileOut.write(buffer, 0, read);
-                            realPercentage = totalRead / length;
-                            String formatted = String.format("%.2f%%", realPercentage * 100);
-                            System.out.println("Current progress: " + formatted);
-                        }
-                    }
-                    //Replace the current installed version with the temp version
-
-                    //Delete all files
-                    Stream.of(siGuiInstallFolder).filter(Files::exists).flatMap(dir -> {
-                        try {
-                            return Files.list(dir);
-                        } catch (IOException e) {
-                            return Stream.empty();
-                        }
-                    }).filter(it -> !it.getFileName().toString().equals(settingFilename)).filter(Files::exists).sorted(Comparator.reverseOrder()).forEach(it -> {
-                        try {
-                            Files.deleteIfExists(it);
-                        } catch (IOException e) {
-                            //Ignore
-                        }
-                    });
-                    //Extract new version
-                    extractZip(tmpFile, siGuiInstallFolder);
-                    //Start SI GUI
-                    startSiGui();
-                    //Save etag
-                    try (BufferedWriter writer = Files.newBufferedWriter(lastUpdatePath, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
-                        writer.write(online.getKey());
-                    }
-                } finally {
-                    Files.deleteIfExists(tmpFile);
-                }
+                downloadAndStartSiGui(online.getKey(), online.getValue());
             }
         } catch (Exception e) {
             e.printStackTrace();
             throw new IllegalStateException("Better exception handling needed!");
+        }
+    }
+
+    private static void downloadAndStartSiGui(String eTag, HttpURLConnection urlConnection) throws Exception {
+        final Path tmpFile = Paths.get(".", eTag + ".zip");
+        Files.deleteIfExists(tmpFile);
+        //TODO show progress / Ask to dowenload
+        try {
+            try (final InputStream onlineIn = urlConnection.getInputStream();
+                 final OutputStream fileOut = Files.newOutputStream(tmpFile, StandardOpenOption.CREATE_NEW)) {
+                final float length = urlConnection.getContentLength();
+                int totalRead = 0;
+                int read;
+                float realPercentage;
+                byte[] buffer = new byte[1024 * 32];
+                while ((read = onlineIn.read(buffer)) >= 0) {
+                    totalRead += read;
+                    fileOut.write(buffer, 0, read);
+                    realPercentage = totalRead / length;
+                    String formatted = String.format("%.2f%%", realPercentage * 100);
+                    System.out.println("Current progress: " + formatted);
+                }
+            }
+            //Replace the current installed version with the temp version
+
+            //Delete all files
+            Stream.of(siGuiInstallFolder).filter(Files::exists).flatMap(dir -> {
+                try {
+                    return Files.list(dir);
+                } catch (IOException e) {
+                    return Stream.empty();
+                }
+            }).filter(it -> !it.getFileName().toString().equals(settingFilename)).filter(Files::exists).sorted(Comparator.reverseOrder()).forEach(it -> {
+                try {
+                    Files.deleteIfExists(it);
+                } catch (IOException e) {
+                    //Ignore
+                }
+            });
+            //Extract new version
+            extractZip(tmpFile, siGuiInstallFolder);
+            boolean canExec = scriptFilePath.toFile().setExecutable(true, false);
+            if (!isWindows && !canExec)
+                throw new IllegalStateException("Cannot make " + scriptFilePath + " executable...");
+            //Start SI GUI
+            startSiGui();
+            //Save etag
+            try (BufferedWriter writer = Files.newBufferedWriter(lastUpdatePath, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
+                writer.write(eTag);
+            }
+        } finally {
+            Files.deleteIfExists(tmpFile);
         }
     }
 
@@ -96,7 +105,7 @@ public class Updater {
     private static String loadCurrentVersionFromDisk() {
         try {
             try (BufferedReader reader = Files.newBufferedReader(lastUpdatePath, StandardCharsets.UTF_8)) {
-                String tmp = reader.readLine();
+                final String tmp = reader.readLine();
                 if (tmp == null)
                     throw new IllegalStateException();
                 return tmp;
@@ -108,11 +117,11 @@ public class Updater {
     }
 
     private static Map.Entry<String, HttpURLConnection> loadLastOnlineVersion() throws IOException {
-        URL url = new URL(updaterUrl);
+        final URL url = new URL(updaterUrl);
 
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        String etag = con.getHeaderField("ETag");
+        final String etag = con.getHeaderField("ETag");
 
         return new AbstractMap.SimpleImmutableEntry(etag.substring(1, etag.length() - 1), con);
     }
@@ -120,19 +129,12 @@ public class Updater {
 
     private static final Path lastUpdatePath = Paths.get(".", "last_update.txt");
 
-    public static void startSiGui() throws Exception {
-        boolean isWindows = System.getProperty("os.name").toLowerCase().contains("wind");
-        final Path scriptFolderPath = siGuiInstallFolder.resolve("si-gui-desktop").resolve("bin");
+    private static void startSiGui() throws Exception {
+        final File execFolder = scriptFilePath.getParent().toFile();
         if (isWindows)
-            new ProcessBuilder("cmd.exe", "/c", "si-gui-desktop.bat").directory(scriptFolderPath.toFile()).inheritIO().start();
+            new ProcessBuilder("cmd.exe", "/c", "si-gui-desktop.bat").directory(execFolder).inheritIO().start();
         else {
-            String filename = "si-gui-desktop";
-            File scriptFolder = scriptFolderPath.toFile();
-            File fileToExecute = new File(scriptFolder, filename);
-            boolean result = fileToExecute.setExecutable(true, false);
-            if (!result)
-                throw new IllegalStateException("Could not set ececute permission!");
-            new ProcessBuilder("sh", "./" + filename).directory(scriptFolder).inheritIO().start();
+            new ProcessBuilder("sh", scriptFilePath.toString()).directory(execFolder).inheritIO().start();
         }
     }
 }
