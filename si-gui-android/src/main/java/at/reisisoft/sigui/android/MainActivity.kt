18c6843d4734ce9ba9d40c4e23ee2ab7bc9a70ea
Florian Reisinger <reisi007@gmail.com>
2018-04-26 14:01:06 +0200

    Added empty android project

---
+++ si-gui-android/src/main/java/at/reisisoft/sigui/android/MainActivity.kt
@@ -0,0 +1,13 @@
+package at.reisisoft.sigui.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import at.reisisoft.sigui.android.R.layout.activity_main
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+    }
+}
