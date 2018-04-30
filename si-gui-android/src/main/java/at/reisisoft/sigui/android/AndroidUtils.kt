8a33ffc4234d2db495116144196705e2200fe375
Florian Reisinger <reisi007@gmail.com>
2018-04-30 09:10:35 +0200

    untested download / install methods

--- si-gui-android/src/main/java/at/reisisoft/sigui/android/AndroidUtils.kt
+++ si-gui-android/src/main/java/at/reisisoft/sigui/android/AndroidUtils.kt
@@ -1,16 +1,78 @@
 package at.reisisoft.sigui.android
 
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
 import android.os.Build
+import android.support.v4.content.FileProvider
 import at.reisisoft.sigui.commons.downloads.DownloadType
+import java.io.File
 
-object AndroidUtils {
+fun getDownloadTypes(): Set<DownloadType> =
+    Build.SUPPORTED_ABIS.asSequence().map {
+        when {
+            it.contains("arm", true) -> DownloadType.ANDROID_LIBREOFFICE_ARM
+            it.contains("x86", true) -> DownloadType.ANDROID_LIBREOFFICE_X86
+            else -> null
+        }
+    }.filterNotNull().plus(DownloadType.ANDROID_REMOTE).toSet()
 
-    fun getDownloadTypes(): Set<DownloadType> =
-        Build.SUPPORTED_ABIS.asSequence().map {
-            when {
-                it.contains("arm", true) -> DownloadType.ANDROID_LIBREOFFICE_ARM
-                it.contains("x86", true) -> DownloadType.ANDROID_LIBREOFFICE_X86
-                else -> null
-            }
-        }.filterNotNull().plus(DownloadType.ANDROID_REMOTE).toSet()
+// https://stackoverflow.com/questions/39147608/android-install-apk-with-intent-view-action-not-working-with-file-provider/40131196#40131196
+fun Activity.installApk(downloadFile: File) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", downloadFile).let {
+            Intent(Intent.ACTION_INSTALL_PACKAGE).apply {
+                setData(it)
+                setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }.also { startActivity(it) }
+        }
+    else {
+        Uri.fromFile(downloadFile).let {
+            Intent(Intent.ACTION_VIEW).apply {
+                setDataAndType(it, "application/vnd.android.package-archive");
+                setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }.also { startActivity(it) }
+            startActivity(intent);
+        }
+    }
 }
+
+private fun String.extractFileName() = substring(lastIndexOf('/') + 1, length)
+
+//https://stackoverflow.com/a/4969421/1870799
+fun Activity.asyncDownloadFile(urlAsString: String) =
+    urlAsString.extractFileName().let { fileName ->
+        File(applicationContext.cacheDir, fileName).let { toFile ->
+            Uri.parse("file://$toFile").let { fileUri ->
+                DownloadManager.Request(Uri.parse(urlAsString)).apply {
+                    getString(R.string.download).let { "$it: ${toFile.name}" }.let { title ->
+                        setDescription(title)
+                        setTitle(title)
+                        setDestinationUri(fileUri)
+                    }
+                }
+            }.let { request ->
+                getSystemService(Context.DOWNLOAD_SERVICE).let {
+                    it as? DownloadManager ?: throw IllegalStateException("DownloadmManager not foun!")
+                }.apply {
+                    enqueue(request).let { downloadId ->
+                        object : BroadcastReceiver() {
+                            override fun onReceive(context: Context, intent: Intent) {
+                                Intent(Intent.ACTION_VIEW).let { i ->
+                                    installApk(toFile)
+                                    unregisterReceiver(this)
+                                    finish()
+                                }
+                            }
+                        }
+                    }.let {
+                        registerReceiver(it, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+                    }
+                }
+            }
+        }
+    }
